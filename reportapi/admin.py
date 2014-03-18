3276fcb0bf7d56586d915ca85459d50b45e7ae90
Grigoriy Kramarenko <root@rosix.ru>
2014-03-18 21:08:04 +1100

    append django admin file

---
+++ reportapi/admin.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+###############################################################################
+# Copyright 2014 Grigoriy Kramarenko.
+###############################################################################
+# This file is part of ReportAPI.
+#
+#    ReportAPI is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    ReportAPI is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with ReportAPI.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Этот файл — часть ReportAPI.
+#
+#   ReportAPI - свободная программа: вы можете перераспространять ее и/или
+#   изменять ее на условиях Стандартной общественной лицензии GNU в том виде,
+#   в каком она была опубликована Фондом свободного программного обеспечения;
+#   либо версии 3 лицензии, либо (по вашему выбору) любой более поздней
+#   версии.
+#
+#   ReportAPI распространяется в надежде, что она будет полезной,
+#   но БЕЗО ВСЯКИХ ГАРАНТИЙ; даже без неявной гарантии ТОВАРНОГО ВИДА
+#   или ПРИГОДНОСТИ ДЛЯ ОПРЕДЕЛЕННЫХ ЦЕЛЕЙ. Подробнее см. в Стандартной
+#   общественной лицензии GNU.
+#
+#   Вы должны были получить копию Стандартной общественной лицензии GNU
+#   вместе с этой программой. Если это не так, см.
+#   <http://www.gnu.org/licenses/>.
+###############################################################################
+"""
+from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
+from .models import *
+
+class RegisterAdmin(admin.ModelAdmin):
+    list_display = ('title', 'section', 'name', 'timeout', 'id')
+    search_fields = ['title', 'id']
+    list_filter = ('section',)
+    filter_horizontal = ('users', 'groups')
+    ordering = ['title']
+admin.site.register(Register, RegisterAdmin)
+
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = ('register', 'id')
+    search_fields = ['register__title', 'id']
+    list_filter = ('register__section', 'register__title')
+    raw_id_fields = ('register', 'user')
+    ordering = ['-end', 'start']
+admin.site.register(Document, DocumentAdmin)
